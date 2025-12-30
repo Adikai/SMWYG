3f6d87c5b775d4333a43dd49b7adac59193317ca
Adikai <adhil.sewrathan@gmail.com>
2025-12-30 23:23:41 +0200

    Moved Converter

---
+++ SMWYG/Converters/StringToInitialsConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SMWYG
+{
+    public class StringToInitialsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string str || string.IsNullOrWhiteSpace(str))
+                return "?";
+
+            var parts = str.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 0) return "?";
+
+            var initials = parts.Length == 1
+                ? parts[0][0].ToString().ToUpper()
+                : parts[0][0].ToString().ToUpper() + parts[^1][0].ToString().ToUpper();
+
+            return initials;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
